f8159e6e18dc15d9d13ab5ef8bfaa7b81557b194
Raphael Adhimas <raphaeldanu@gmail.com>
2026-08-28 23:19:15 +0700

    add: Add transaction category seeder and change the redirect on create and edit after saving

--- app/Filament/Resources/TransactionCategories/Pages/CreateTransactionCategory.php
+++ app/Filament/Resources/TransactionCategories/Pages/CreateTransactionCategory.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateTransactionCategory extends CreateRecord
 {
     protected static string $resource = TransactionCategoryResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
---
+++ database/seeders/TransactionCategorySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\TransactionCategory;
+use Illuminate\Database\Seeder;
+
+class TransactionCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            'Offerings',
+            'Utilities',
+            'Subscription',
+            'Entertainment',
+            'Groceries',
+            'Health',
+            'Transport',
+            'Savings'
+        ];
+
+        foreach($categories as $category) {
+            TransactionCategory::create(['name' => $category]);
+        }
+    }
+}
